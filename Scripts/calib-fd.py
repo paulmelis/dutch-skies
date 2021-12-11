c52ae0728292bf02fefaf04b12b021c67634e697
Paul Melis <p.e.c.melis@alumnus.utwente.nl>
2021-12-11 19:43:46 +0100

    Realized the solver was optimizing for the wrong transform. It should find the transform that maps the reference points onto the observations... Test script works now, need to update the C# code as well

--- Scripts/calib-fd.py
+++ Scripts/calib-fd.py
@@ -24,6 +24,14 @@ def line_point_distance_xz(p, q, t):
     den = sqrt((x2-x1)**2 + (z2-z1)**2)
     return num/den
     
+def behind_xz(p, q, t):
+    """Return if point t is behind the observer at p (who is viewing from p to q).
+    Ignores Y"""
+    v = (q[0]-p[0], q[1]-p[1], q[2]-p[2])
+    w = (t[0]-p[0], t[1]-p[1], t[2]-p[2])
+    return v[0]*w[0] + v[2]*w[2] < 0
+    
+    
 def rot_y(p, angle_degrees):
     """Positive rotation = CCW"""
     a = radians(angle_degrees)
@@ -51,23 +59,43 @@ if False:
     
     
 def compute_energy(tx, ty, tz, r):
-    """RMS"""
+    """Returns RMS in meters"""
     
     e2 = 0.0
-    n = 0
-    for ob_origin, name, direction in OBSERVATIONS:
-        #print(name)
-        o2 = rot_y(ob_origin, r)
-        o2 = (o2[0] + tx, o2[1] + ty, o2[2] + tz)
-        p2 = rot_y((ob_origin[0]+direction[0], ob_origin[1]+direction[1], ob_origin[2]+direction[2]), r)
-        p2 = (p2[0]+tx, p2[1]+ty, p2[2]+tz)
-        #print(ob_origin, '->', o2)
-        #print(direction, '->', p2)
-        dist = line_point_distance_xz(o2, p2, GROUND_TRUTH[name])
-        #print(dist)
-        e2 += dist*dist
-        n += 1
         
+    for ref_name, ref_position in GROUND_TRUTH.items():
+        #print(ref_name)
+        
+        r2 = rot_y(ref_position, r)
+        r2 = (r2[0] + tx, r2[1] + ty, r2[2] + tz)
+        
+        n = 0
+        behind = 0
+        for observation in OBSERVATIONS:            
+            if observation['lm'] != ref_name:
+                # XXX yuck
+                continue            
+            ob_origin = observation['head_pos']
+            ob_direction = observation['head_ori']
+            ob2 = (ob_origin[0] + ob_direction[0], ob_origin[1] + ob_direction[1], ob_origin[2] + ob_direction[2])
+            dist = line_point_distance_xz(ob_origin, ob2, r2)
+            
+            # Penalize solution when point is behind, as seen along viewing direction
+            if behind_xz(ob_origin, ob2, r2):
+                dist += 1000
+                
+            #max_dist = max(max_dist, dist)
+            #print(observation['lm'], dist)
+            e2 += dist*dist
+            
+        n += 1
+            
+        if n == 0:
+            print('WARNING: no observations for "%s"' % ref_name)
+                    
+        #print(max_dist)
+        
+    #return max_dist
     return sqrt(e2/n)
     
 def rand_unit():
@@ -82,33 +110,24 @@ if __name__ == '__main__':
         'RT': (-891.0309, 148, 701.9933), 
         'HKF': (222.3811, 33.5, -189.894100),
         'HKB': (259.083, 30.5, -178.2989),
-        'BR': (303.6142, 30.5, 79.835310),
+        'BRN': (303.6142, 30.5, 79.835310),
         'DHL': (-476.4835, 64, 254.3338),
         'SS': (-52.751860, 11, -22.81),
-        'BOOM': (50.029740, 9, 115.9509)
-    }
-    
-    # Direction vectors from observer position
+        #'Tree': (50.029740, 9, 115.9509)
+    }    
+
+    # Observations as position + direction vector.
     # Note: head pos Y (height) is relative to starting head position
     # and not above floor
     OBSERVATIONS = [
-        ((1.070687, 0.084614, 3.150276), 'DHL', (0.922612, 0.123531, -0.365413)),
-        ((1.187483, 0.075501, 3.316733), 'SS', (0.762326, 0.122043, 0.635582)),
-        ((1.207898, 0.053789, 3.258828), 'BOOM', (-0.362435, 0.037529, -0.931253)),
-        ((2.160876, 0.044619, -5.497616), 'BOOM', (-0.394080, 0.017386, -0.918912)),
-        ((2.198612, 0.063130, -5.415352), 'SS', (0.691618, 0.079084, 0.717921)),
-        ((2.145756, 0.059008, -5.169587), 'HKF', (-0.792862, 0.044595, 0.607767)),
-        ((1.912785, 0.056219, -5.512104), 'RT', (0.844261, 0.071267, -0.531173))
+        {"lm":"HKB","head_pos":[-0.144786, 0.057414, 0.179659],"head_ori":[0.166631, 0.251750, 0.953340]},
+        {"lm":"HKF","head_pos":[-0.106714, 0.054435, 0.165516],"head_ori":[0.567109, 0.214798, 0.795142]},
+        {"lm":"RT","head_pos":[1.752961, 0.516924, -8.370631],"head_ori":[0.206662, 0.068152, -0.976036]},
+        {"lm":"DHL","head_pos":[1.885210, 0.535351, -8.261091],"head_ori":[0.358756, 0.031792, -0.932890]},
+        {"lm":"SS","head_pos":[1.580237, 0.554369, -9.275459],"head_ori":[0.530321, -0.018952, -0.847585]},
+        {"lm":"BRN","head_pos":[2.233647, 0.309135, -9.938712],"head_ori":[-0.945653, 0.053625, -0.320727]},
+        {"lm":"SS","head_pos":[-0.193687, 0.547925, -6.495636],"head_ori":[0.534311, -0.042488, -0.844219]}
     ]
-    
-    """
-    print(compute_energy(tx, ty, r))
-    print(compute_energy(tx, ty, r+90))
-    print(compute_energy(tx, ty, r+180))    
-    print(compute_energy(tx, ty, r+270))
-    print(compute_energy(-2.972659, -3.278703, 155.837377-180))
-    print(compute_energy(-2.972659, -3.278703, 155.837377))    
-    """
     
     # Compute the transformation rot(R) followed by T(tx,ty,tz)
     # that maps the sky coordinate system (in which the ground truth
@@ -124,13 +143,15 @@ if __name__ == '__main__':
     best_tz = tz
     best_r = r
     best_energy = compute_energy(tx, ty, tz, r)
-    print('Energy %.6f (initial) | tx=%.6f, ty=%.6f, tz=%.6f, r=%.6f' % \
+    print('[initial] Energy %.6f | tx=%.6f, ty=%.6f, tz=%.6f, r=%.6f' % \
         (best_energy, best_tx, best_ty, best_tz, best_r))
     
-    K = 40000
-    WO = 200
+    K = 50000
+    WO = 2000
     without_improvement = 0
+    
     for k in range(0, K):
+        
         T = 1 - (k+1)/K
         #print('Iter %d, T = %.6f' % (k, T))
         
@@ -143,21 +164,27 @@ if __name__ == '__main__':
         idx = randint(0, 2)
         if idx == 0:
             #print('Mutating tx')            
-            can_tx = tx + rand_unit()*200*T
+            can_tx = tx + rand_unit()*2000*T
         # NOTE: ty never mutated
         elif idx == 1:
             #print('Mutating tz')
-            can_tz = tz + rand_unit()*200*T
+            can_tz = tz + rand_unit()*2000*T
         elif idx == 2:
             #print('Mutating r')
             can_r = (r + rand_unit()*360*T) % 360
+        #elif idx == 3:
+        #    # Negate all values
+        #    can_tx = -tx
+        #    can_tz = -tz
+        #    can_r = -r
             
         energy = compute_energy(can_tx, can_ty, can_tz, can_r)
-        #print('Energy %.6f (candidate) | can_tx=%.6f, can_ty=%.6f, can_tz=%6f, can_r=%.6f' % \
-        #    (energy, can_tx, can_ty, can_tz, can_r))
+        
+        #print('[%05d] Energy %8.3f (candidate, idx %d) | can_tx=%.6f, can_ty=%.6f, can_tz=%6f, can_r=%.6f' % \
+        #    (k, energy, idx, can_tx, can_ty, can_tz, can_r))
             
-        if energy <= best_energy:
-            print('[%d] Energy %.6f (new best) | tx=%.6f, ty=%.6f, tz=%6f, r=%.6f' % \
+        if energy < best_energy:
+            print('[%05d] Energy %8.3f (new best)         | tx=%.6f, ty=%.6f, tz=%6f, r=%.6f' % \
                 (k, energy, can_tx, can_ty, can_tz, can_r))
             best_energy = energy
             best_tx = can_tx
@@ -165,11 +192,15 @@ if __name__ == '__main__':
             best_tz = can_tz
             best_r = can_r
             without_improvement = 0
+            tx = can_tx
+            ty = can_ty
+            tz = can_tz
+            r = can_r
         elif T > 0:
             without_improvement += 1
             if without_improvement == WO:
                 # Restart with last best solution
-                #print('Restarting')
+                print('[%05d] %d steps without improvement, restarting' % (k, WO))
                 tx = best_tx
                 ty = best_ty
                 tz = best_tz
@@ -180,7 +211,7 @@ if __name__ == '__main__':
             #print(energy, best_energy, T)
             p = exp(-(energy-best_energy)/T)
             if random() <= p:
-                #print('Transitioning to less optimal solution')
+                #print('Transitioning to less optimal solution (p=%.6f)' % p)
                 tx = can_tx
                 ty = can_ty
                 tz = can_tz
